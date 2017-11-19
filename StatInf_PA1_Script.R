e9c8c77c5a7579bf1dd162af9b24879996e0f369
sherifffruitfly <sherifffruitfly@gmail.com>
2017-11-19 14:57:53 -0800

    project started.

---
+++ StatInf_PA1_Script.R
@@ -0,0 +1,21 @@
+# script for statinf project
+
+# assign simulation parameters
+set.seed(42)
+lam = .5
+n <- 40
+simulations <- 1000
+
+# run & plot the experiment
+exp_means <- NULL
+for (i in 1:simulations) {
+  exp_means <- c(exp_means, mean(rexp(n, lam)))
+}
+hist(exp_means)
+
+mean_sample <- mean(exp_means)
+mean_theory <- 1/lam
+
+var_sample <- var(exp_means)
+var_theory <- 1/(lam^2 * n)
+#y <- dnorm(x, 1, 0)
