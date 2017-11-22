255b844374cf5463fdd0c01d0be34315c231dc26
seedjay1 <see_d_jay@yahoo.com>
2017-11-22 09:44:16 -0800

    toothgrowth code/document structure created.

---
+++ StatInf1_PA1_ToothGrowth_Script.R
@@ -0,0 +1,27 @@
+# ToothGrowth data analysis
+# first we practice code externalization
+
+## @knitr chunk_1
+# chunk 1 code
+library(datasets)
+data(ToothGrowth)
+
+str(ToothGrowth)
+head(ToothGrowth, 5)
+summary(ToothGrowth)
+
+
+## @knitr chunk_2
+# chunk 2 code
+2+2
+x <- 2+1
+
+
+## @knitr chunk_3
+# chunk 3 code
+y <- x + 4
+
+
+## @knitr chunk_4
+# chunk 4 code
+x + y
